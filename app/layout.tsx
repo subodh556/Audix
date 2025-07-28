c31b0079ecad6ed667361b6eea0201755e360d9d
Subodh <sohonisubodh@gmail.com>
2025-07-28 11:36:28 +0530

    Auth Modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import {Figtree} from "next/font/google"
 import Sidebar from "@/components/Sidebar";
 import SupabaseProvider from "@/providers/SupabaseProvider";
 import UserProvider from "@/providers/UserProvider";
+import ModalProvider from "@/providers/ModalProvider";
+import ToasterProvider from "@/providers/ToasterProvider";
 
 const inter = Figtree({subsets: ['latin']})
 
@@ -22,8 +24,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <ToasterProvider/>
         <SupabaseProvider>
             <UserProvider>
+                <ModalProvider />
                 <Sidebar>
                   {children}
                 </Sidebar>
